ffeba99c8f24b88ab77d964814b616a70cf90782
Hongchang Wu <wuhc85@gmail.com>
2014-11-28 00:56:11 -0500

    change pattern order

--- problem61.hs
+++ problem61.hs
@@ -1,6 +1,6 @@
 import Tree
 
 countLeaves :: Tree a -> Int
-countLeaves (Branch _ Empty Empty) = 1
 countLeaves Empty = 0
+countLeaves (Branch _ Empty Empty) = 1
 countLeaves (Branch _ u v) = countLeaves u + countLeaves v
