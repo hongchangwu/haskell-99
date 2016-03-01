61331cad87fc45b335c36a8ed0ca0e1954fabbcb
Hongchang Wu <Hongchang.Wu@morganstanley.com>
2016-02-29 22:32:08 -0500

    Problem 80 - part 2

--- problem80.hs
+++ problem80.hs
@@ -1,3 +1,5 @@
+import Control.Applicative (liftA2)
+import Data.List ((\\))
 import Data.Maybe (mapMaybe)
 import Graph
 
@@ -18,3 +20,27 @@ adjToGraph (Adj adjs) = Graph ns es
   where
     (ns, es) = foldr f ([], []) adjs
     f (x, xs) (ns, es) = (x : ns, (map (\y -> (x, y)) . filter (`elem` ns)) xs ++ es)
+
+graphToFri :: (Eq a) => Graph a -> Friendly a
+graphToFri (Graph ns es) = Edge $ concatMap f ns
+  where
+    f n = case filter (liftA2 (||) ((==) n . fst) ((==) n . snd)) es of
+      [] -> [(n, n)]
+      xs -> filter ((== n) . fst) xs
+
+friToGraph :: (Eq a) => Friendly a -> Graph a
+friToGraph (Edge xs) = Graph ns es
+  where
+    (ns, es) = loop [] [] xs
+      where
+        loop ns es [] = (ns, es)
+        loop ns es ((a, b) : ys) = let ns' = ns ++ if a == b then [a] else [a, b] \\ ns
+                                       es' = es ++ if a == b then [] else [(a, b)]
+                                       ys' = filter (\(x, y) -> (x /= a || y /= b) && (x /= b || y /= a)) ys
+                                   in loop ns' es' ys'
+    
+adjToFri :: (Eq a) => Adjacency a -> Friendly a
+adjToFri = graphToFri . adjToGraph
+
+friToAdj :: (Eq a) => Friendly a -> Adjacency a
+friToAdj = graphToAdj . friToGraph
