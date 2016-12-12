d3bad7ce94256683fc84e2674c65372b11e8367c
Hongchang Wu <wuhc85@gmail.com>
2016-12-11 22:55:06 -0500

    Rewrite n queens using foldM

--- problem90.hs
+++ problem90.hs
@@ -1,21 +1,11 @@
-import Control.Monad (guard)
-import Data.List (delete)
+import Control.Monad (foldM)
+import Data.List
 
 queens :: Int -> [[Int]]
-queens n = queens' [([], ks)] 0
+queens n = map reverse $ foldM next [] [1..n]
   where
-    ks = take n [0 ..]
-    queens' zss j
-      | j >= n = map (map (+ 1) . reverse . fst) zss
-      | otherwise =
-        let zss' = do
-              (xs, ys) <- zss
-              i <- ys
-              guard $ safe i xs
-              return (i : xs, delete i ys)
-        in queens' zss' (succ j)
-      where
-        safe i xs = and [i /= r + n && i /= r - n | (n, r) <- zip [1..] xs]
+    next xs j = [(i : xs) | i <- [1..n] \\ xs, safe i xs]
+    safe i xs = and [i /= r + n && i /= r - n | (n, r) <- zip [1..] xs]
 
 main :: IO ()
 main = do
