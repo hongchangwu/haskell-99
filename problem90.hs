e9189438584652af7a9b03f9d000c497da21914d
Hongchang Wu <wuhc85@gmail.com>
2016-12-17 13:59:59 -0500

    _

--- problem90.hs
+++ problem90.hs
@@ -4,7 +4,7 @@ import Data.List
 queens :: Int -> [[Int]]
 queens n = map reverse $ foldM next [] [1..n]
   where
-    next xs j = [(i : xs) | i <- [1..n] \\ xs, safe i xs]
+    next xs _ = [(i : xs) | i <- [1..n] \\ xs, safe i xs]
     safe i xs = and [i /= r + n && i /= r - n | (n, r) <- zip [1..] xs]
 
 main :: IO ()
