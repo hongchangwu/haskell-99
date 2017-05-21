e743da541bdd2f1fdddf3483884298cae53cf1c5
Hongchang Wu <wuhc85@gmail.com>
2017-05-21 16:37:45 -0400

    Add isCompleteBinaryTree

--- app/problem63/Main.hs
+++ app/problem63/Main.hs
@@ -10,6 +10,13 @@ completeBinaryTree n = numberedTree 1
       | (2 * x <= n) = Branch 'x' (numberedTree (2 * x)) Empty
       | otherwise = Branch 'x' Empty Empty
 
+isCompleteBinaryTree :: Tree Char -> Bool
+isCompleteBinaryTree t = t == completeBinaryTree n
+  where
+    n = count t
+
 main :: IO ()
 main = do
   print $ completeBinaryTree 4
+  print . isCompleteBinaryTree $
+    Branch 'x' (Branch 'x' Empty Empty) (Branch 'x' Empty Empty)
